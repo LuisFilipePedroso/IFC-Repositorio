0197eef4870252b56aafd03f8bf9cf83ab4e1f97
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2019-10-11 10:09:18 -0300

    Argon theme

--- dashboard/src/components/Header/index.js
+++ dashboard/src/components/Header/index.js
@@ -1,42 +1,52 @@
 // React & Redux
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 // Styles
+// import { SidebarToggler } from './style'
 import { Button, Collapse, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, NavbarBrand, NavbarToggler, NavLink, NavItem, UncontrolledDropdown } from 'reactstrap'
 
 // Header
 const Header = () => {
+  const [open, isOpen] = useState(false)
+
+  /**
+   * Toggle mobile menu
+   */
+  const toggle = () => isOpen(!open)
+
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <Navbar color="dark" light expand="md" className="bg-gradient-primary">
+      <Button color=" white">Menu</Button>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={open} navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/">GitHub</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Options
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                Option 1
+              </DropdownItem>
+              <DropdownItem>
+                Option 2
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                Reset
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
   )
 }
 
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-// Others
-import 'bootswatch/dist/minty/bootstrap.min.css'
+// Argon theme
+import './static/css/argon-dashboard.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
