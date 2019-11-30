ecf432d55048c0020942ef6daae8e448fabd09d5
frontorio <54674396+frontorio@users.noreply.github.com>
2019-11-30 15:06:03 -0300

    Alterações página de upload

--- site/resources/views/upload.blade.php
+++ site/resources/views/upload.blade.php
@@ -6,12 +6,18 @@
                 <div class="col-md-2" id="menuLateral">
                     <ul class="nav nav-pills flex-column">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Meu perfil</a>
+                            <a class="nav-link active" href="#">Meu perfil</a>
                             <a class="nav-link" href="#">Meus trabalhos</a>
                             <a class="nav-link" href="#">Enviar novo trabalho</a>
                             <a class="nav-link" href="#">Dashboard</a>
                         </li>
                      </ul>
+                </div>
+                <div class="col-md-8" id="cont">
+
+                    <img id='imgProf' src="{{ asset('img/icon-profile/icon-login-default.png') }}">
+                    <input type="text" id="campoProf" readonly value="Nome do usuário">
+
                 </div>
         </div>
     </div>
