0999b65e03e27b98fc2016f335e2702ab5e25c54
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2020-01-14 22:34:39 -0300

    [UPD] Atualizando a página de visualização de um trabalho

--- site/app/Http/Controllers/TrabalhoController.php
+++ site/app/Http/Controllers/TrabalhoController.php
@@ -2,42 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use Datetime;
+
 class TrabalhoController extends Controller
 {
     public function index($id)
     {
-        // Realiza um get, buscando o trabalho (article) com o id = $id
-        $jsonTrabalho = '
-            {
-                "id": 123456,
-                "event_id": 123,
-                "title": "WSFTA: uma arquitetura para tolerância a faltas em serviços web",
-                "abstract": "A grande vantagem na utilização de ServiçosWeb para concepção de sistemas distribuídos é a interoperabilidade que essa tecnologia proporciona [Ayala et al., 2002]. Essa interoperabilidade facilita de forma significativa o desenvolvimento de aplicações distribuídas, já que problemas tais como a diversidade de hardwares e softwares são solucionados pelos protocolos utilizados por esses serviços [Newcomer, 2002]. Nesse sentido, Serviços Web têm sido amplamente utilizados para solucionar problemas de interoperabilidade entre aplicações e/ou tecnologias. Porém, as especificações e padrões definidos para Serviços Web não solucionam problemas relativos a tolerância a faltas dos serviços. A forma mais tradicional para tolerar faltas é a utilização de esquemas de replicação, provendo continuidade e disponibilidade aos serviços [Veríssimo and Rodrigues, 2001, Li et al., 2005]. Baseado nessa premissa, esse trabalho apresenta uma proposta de arquitetura de software para tolerância a faltas em Serviços Web. Essa arquitetura, chamada de WSFTA (Web Services Fault Tolerant Architecture), descreve de forma abstrata seus componentes e princípio de funcionamento, dividindo as responsabilidades e funcionalidades, presentes na arquitetura, entre esses componentes. Adicionalmente, este trabalho apresenta a especificação formal desses componentes, que possibilita a utilização de diferentes técnicas para comunicação de grupo, ordenação de mensagens, detecção e recuperação de falhas, proporcionando a flexibilidade necessária para que a arquitetura possa ser configurada para tratar as diversas classes de faltas presentes na literatura. Além disso, esse trabalho ainda apresenta uma implementação de referência e experimentos que possibilitam a comprovação de sua potencialidade de utilização no fornecimento de tolerância a faltas para Serviços Web.",
-                "altabstract": "The major advantage of using Web Services for distributed system design is the interoperability this technology provides [Ayala et al., 2002]. This interoperability significantly facilitates the development of distributed applications, as problems such as hardware and software diversity are solved by the protocols used by these services [Newcomer, 2002]. In this sense, Web services have been widely used to solve interoperability problems between applications and / or technologies. However, the specifications and standards defined for Web Services do not solve service fault tolerance issues. The most traditional way to tolerate faults is the use of replication schemes, providing continuity and availability to services [Veríssimo and Rodrigues, 2001, Li et al., 2005]. Based on this premise, this paper presents a software architecture proposal for fault tolerance in Web Services. This architecture, called Web Services Fault Tolerant Architecture (WSFTA), abstractly describes its components and principle of operation, dividing responsibilities and functionalities present in the architecture between these components. Additionally, this paper presents the formal specification of these components, which allows the use of different techniques for group communication, message ordering, fault detection and recovery, providing the necessary flexibility so that the architecture can be configured to handle the various classes of communication. faults present in the literature. In addition, this work also presents a reference implementation and experiments that provide evidence of its potential use in providing fault tolerance for Web Services.",
-                "keywords": "Ciência da Computação; TCC;",
-                "tags": "Tag 01; Tag 02; Tag 03;",
-                "year": "2008",
-                "date": "04-11-2019",
-                "language": "Português",
-                "type": "TCC",
-                "aproved": "true",
-                "file": "arquivo",
-                "url": "http://google.com"
-            }
-        ';
-        // Decodifica retornando um array (não um stdClass)
-        $aTrabalho = json_decode($jsonTrabalho, true);
-
-        $aUsuario = ['firstname' => 'Mathias Artur', 'lastname' => 'Schulz'];
-
-        $aCurso = ['name' => 'Ciência da Computação'];
-
-        $aEvento = [
-            'name' => '4C - Congresso Catarinense de Ciência da Computação',
-            'year' => 2019,
-            'url' => 'http://google.com'
-        ];
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => 'localhost:3333',
+            'timeout'  => 2.0,
+        ]);
         
-        return view('trabalho', compact('aTrabalho', 'aUsuario', 'aCurso', 'aEvento'));
+        // Busca o trabalho
+        $response = $client->request('GET', '/articles/' . $id);
+        $trabalho = json_decode($response->getBody(), true);
+        
+        // Não encontrou trabalho com o id determinado
+        if (is_null($trabalho)) {
+            return view('home');
+        }
+
+        // Busca os artigos
+        $cursos = $this->getCoursesFromArticle($client, $id);
+
+        // Busca os usuários
+        $usuarios = $this->getUsersFromArticle($client, $id);
+
+        // Busca o evento
+        $response = $client->request('GET', '/events/' . $trabalho['event_id']);
+        $evento = json_decode($response->getBody(), true);
+
+        // Formata a data de publicação
+        $date = new Datetime($trabalho['date']);
+        $trabalho['date'] = $date->format('d/m/Y');
+        
+        return view(
+            'trabalho', 
+            compact('trabalho', 'usuarios', 'cursos', 'evento')
+        );
+    }
+
+    /**
+     * Busca todos os cursos de um artigo, retornando 'id' e 'name' de cada curso
+     */
+    private function getCoursesFromArticle($client, $id)
+    {
+        $response = $client->request('GET', '/coursesarticles');
+        $todosTrabalhos = json_decode($response->getBody());
+
+        // Busca os cursos do artigo
+        $cursos = [];
+        foreach ($todosTrabalhos as $trabalho) {
+            if ($trabalho->article_id == $id) {
+                $cursos[] = [
+                    'id'   => $trabalho->{'Courses.id'},
+                    'name' => $trabalho->{'Courses.name'}
+                ];
+            }
+        }
+
+        return $cursos;
+    }
+
+    /**
+     * Busca todos os usuários de um artigo
+     * Retornando 'id', 'firstname' e 'lastname' de cada usuario
+     */
+    private function getUsersFromArticle($client, $id)
+    {
+        $response = $client->request('GET', '/usersarticles');
+        $todosUsuarios = json_decode($response->getBody());
+
+        // Busca os usuários do artigo
+        $usuarios = [];
+        foreach ($todosUsuarios as $usuario) {
+            if ($usuario->article_id == $id) {
+                $usuarios[] = [
+                    'id'        => $usuario->Users[0]->id,
+                    'firstname' => $usuario->Users[0]->firstname,
+                    'lastname'  => $usuario->Users[0]->lastname
+                ];
+            }
+        }
+
+        return $usuarios;
     }
 }
--- site/resources/views/trabalho.blade.php
+++ site/resources/views/trabalho.blade.php
@@ -4,51 +4,73 @@
 <div id="container-trabalho" class="container-fluid">
     <div class='container'>
         <div class="titulo-pagina"> 
-            <h2>{{$aTrabalho['title']}}</h2>
+            <h2>{{$trabalho['title']}}</h2>
         </div>
 
         <div class="row">
             <div class="col-sm-3">
-                <div class="titulo">Autor</div>
+                <div class="titulo">Autores</div>
                 <div class="conteudo">
-                    <a href="#" target="_blank">{{$aUsuario['firstname']}} {{$aUsuario['lastname']}}</a>
+                    @foreach ($usuarios as $usuario)
+                        <p>{{$usuario['firstname']}} {{$usuario['lastname']}}</p>
+                        @if (!$loop->last)
+                            <br>
+                        @endif
+                    @endforeach
                 </div>
                 <div class="titulo">Tipo de Trabalho</div>
-                <div class="conteudo">{{$aTrabalho['type']}}</div>
+                <div class="conteudo">{{$trabalho['type']}}</div>
                 <div class="titulo">Abrir documento</div>
-                <div class="conteudo"><a href="{{$aTrabalho['url']}}" target="_blank">
-                    <img id='documento' src="{{ asset('img/documento.png') }}" alt="pdf"> {{$aTrabalho['url']}}
+                <div class="conteudo"><a href="{{$trabalho['url']}}" target="_blank">
+                    <img id='documento' src="{{ asset('img/documento.png') }}" alt="pdf"> {{$trabalho['url']}}
                 </a></div>
                 <div class="titulo">Data de Publicação</div>
-                <div class="conteudo">{{$aTrabalho['date']}}</div>
+                <div class="conteudo">{{$trabalho['date']}}</div>
                 <div class="titulo">Palavras-chave</div>
-                <div class="conteudo">{{$aTrabalho['keywords']}}{{$aTrabalho['tags']}}</div>
+                <div class="conteudo">
+                    {{$trabalho['keywords']}}
+                    @if ($trabalho['keywords'] and $trabalho['tags'])
+                        <br>
+                    @endif
+                    {{$trabalho['tags']}}
+                </div>
                 <div class="titulo">Linguagem</div>
-                <div class="conteudo">{{$aTrabalho['language']}}</div>
-                
+                <div class="conteudo">{{$trabalho['language']}}</div>
             </div>
+
             <div class="col-sm-9">
                 <div class="titulo">Resumo</div>
-                <div class="conteudo texto">{{$aTrabalho['abstract']}}</div>
+                <div class="conteudo texto">{{$trabalho['abstract']}}</div>
                 <div class="titulo">Abstract</div>
-                <div class="conteudo texto">{{$aTrabalho['altabstract']}}</div>
+                <div class="conteudo texto">{{$trabalho['altabstract']}}</div>
                 <div class="row">
                     <div class="col-sm-5">
-                        <div class="titulo">Curso</div>
-                        <div class="conteudo">{{$aCurso['name']}}</div>
+                        <div class="titulo">Cursos</div>
+                        <div class="conteudo">
+                            @foreach ($cursos as $curso)
+                                {{$curso['name']}}
+                                @if (!$loop->last)
+                                    <br>
+                                @endif
+                            @endforeach
+                        </div>
                     </div>
                     <div class="col-sm-7">
                         <div class="titulo">Evento de publicação</div>
                         <div class="conteudo">
-                            <a href="{{$aEvento['url']}}" target="_blank">
-                                {{$aEvento['name']}} - {{$aEvento['year']}}<br>
-                            </a>
+                            @if ($evento['url'])
+                                <a href="{{$evento['url']}}" target="_blank">
+                                    {{$evento['name']}} - {{$evento['year']}}
+                                </a>
+                            @else
+                                <p>{{$evento['name']}} - {{$evento['year']}}</p>
+                            @endif
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <br><br><br><br>
+        <br><br>
     </div>
 </div>
 
