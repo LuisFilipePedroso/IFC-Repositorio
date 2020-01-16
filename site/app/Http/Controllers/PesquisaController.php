5a4c20c742abba8e0f5251a73a5e5798e000a50b
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2020-01-15 22:13:57 -0300

    [UPD] Listando os trabalhos na página de busca de trabalhos

--- site/app/Http/Controllers/PesquisaController.php
+++ site/app/Http/Controllers/PesquisaController.php
@@ -8,12 +8,59 @@ class PesquisaController extends Controller
 {
     public function pesquisar(Request $request)
     {
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => 'localhost:3333',
+            'timeout'  => 2.0,
+        ]);
+
         // Pegar todo o request
         // var_dump($request->all());
-
         // Pega o request get 'nome'
-        echo $request->input('nome');
+        $pesquisa = $request->input('nome');
 
-        return "\n\nEntrou na pesquisa de trabalhos";
+        // Busca os trabalhos
+        $response = $client->request('GET', '/articles');
+        $trabalhos = json_decode($response->getBody(), true);
+
+        // APLICAR OS FILTROS DE BUSCA
+
+        
+        // Busca os usuários de cada artigo
+        foreach ($trabalhos as &$trabalho) {
+            $trabalho['usuarios'] = $this->getUsersFromArticle($client, $trabalho['id']);
+        }
+
+        // var_dump($trabalhos);
+        
+        // // Não encontrou trabalho com o id determinado
+        // if (is_null($trabalho)) {
+        //     return view('home');
+        // }
+
+        return view(
+            'pesquisa', 
+            compact('pesquisa', 'trabalhos')
+        );
+    }
+
+    /**
+     * Busca todos os usuários de um artigo
+     * Retorna um array com os nomes dos usuários
+     */
+    private function getUsersFromArticle($client, $id)
+    {
+        $response = $client->request('GET', '/usersarticles');
+        $todosUsuarios = json_decode($response->getBody());
+
+        // Busca os usuários do artigo
+        $usuarios = [];
+        foreach ($todosUsuarios as $usuario) {
+            if ($usuario->article_id == $id) {
+                $usuarios[] = $usuario->Users[0]->firstname
+                        . ' ' . $usuario->Users[0]->lastname;
+            }
+        }
+
+        return $usuarios;
     }
 }
--- site/resources/views/pesquisa.blade.php
+++ site/resources/views/pesquisa.blade.php
@@ -6,8 +6,17 @@
         <div class="titulo-pagina"> 
             <h2>Pesquisa</h2>
         </div>
+        <p>Busca de: {{$pesquisa}}</p>
+        <div class="conteudo">
+            @foreach ($trabalhos as $trabalho)
+                <div>
+                    <p><a href="/trabalho/{{$trabalho['id']}}">{{$trabalho['title']}}</a></p>
+                    <p>{{$trabalho['abstract']}}</p>
+                </div>
+            @endforeach
+        </div>
 
-        <br><br>
+        <br><br><br><br><br>
     </div>
 </div>
 
