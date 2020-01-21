9216e31d99f0b6ea694d667d23398b3acc713834
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2020-01-21 20:36:26 -0300

    [ADD] Montando os filtros em php

--- site/app/Http/Controllers/PesquisaController.php
+++ site/app/Http/Controllers/PesquisaController.php
@@ -14,21 +14,44 @@ class PesquisaController extends Controller
         ]);
 
         // Pegar todo o request
-        // var_dump($request->all());
+        // foreach ($request->all() as $a) {
+        //     echo $a;
+        // }
+        // foreach($request->all() as $key => $val) {
+        //     echo $key. ': ' .$val;
+        // }
+        // var_dump($request);
+        // var_dump($_GET);
+
+        // $cod_deptos = $_GET['mathias'];
+        // var_dump($cod_deptos);
+
+        $result = self::gerarFiltros();
+        var_dump($result);
+
+        // foreach($cod_deptos as $cod_depto){
+        //     echo $cod_depto . "<br />";
+        // }
+
+        // parse_str($request->all(), $output);
+
+        // //Exibe o resultado
+        // print_r($output);
+
         // Pega o request get 'nome'
-        $pesquisa = $request->input('nome');
+        // $pesquisa = $request->input('nome');
 
-        // Busca os trabalhos
-        $response = $client->request('GET', '/articles');
-        $trabalhos = json_decode($response->getBody(), true);
+        // // Busca os trabalhos
+        // $response = $client->request('GET', '/articles');
+        // $trabalhos = json_decode($response->getBody(), true);
 
-        // APLICAR OS FILTROS DE BUSCA
+        // // APLICAR OS FILTROS DE BUSCA
 
         
-        // Busca os usuários de cada artigo
-        foreach ($trabalhos as &$trabalho) {
-            $trabalho['usuarios'] = $this->getUsersFromArticle($client, $trabalho['id']);
-        }
+        // // Busca os usuários de cada artigo
+        // foreach ($trabalhos as &$trabalho) {
+        //     $trabalho['usuarios'] = $this->getUsersFromArticle($client, $trabalho['id']);
+        // }
 
         // var_dump($trabalhos);
         
@@ -37,10 +60,10 @@ class PesquisaController extends Controller
         //     return view('home');
         // }
 
-        return view(
-            'pesquisa', 
-            compact('pesquisa', 'trabalhos')
-        );
+        // return view(
+        //     'pesquisa', 
+        //     compact('pesquisa', 'trabalhos')
+        // );
     }
 
     /**
@@ -62,5 +85,109 @@ class PesquisaController extends Controller
         }
 
         return $usuarios;
+    }
+
+    public function gerarFiltros()
+    {
+        $request = self::getRequestGet();
+        // var_dump($request);
+
+        // VALIDAR QUANDO FOR APENAS O NOME
+        if (!$request) {
+            return [1, request];
+        }
+        
+        $filtros = self::montaFiltros($request);
+        if (!$filtros) {
+            return false;
+        }
+
+        if (!self::validaFiltros($filtros)) {
+            return false;
+        }
+        return [2, $filtros];
+    }
+
+    /**
+     * Método responsável por ler o request GET e retornar os parâmetros
+     */
+    public function getRequestGet()
+    {
+        if (!isset($_SERVER['QUERY_STRING'])) {
+            return false;
+        }
+        $request = $_SERVER['QUERY_STRING'];
+        $request = urldecode($request);
+        $request = explode("&", $request);
+        $filtros = [];
+        foreach ($request as $key => $value) {
+            $filtros[] = explode("=", $value);
+        }
+        return $filtros;
+    }
+
+    /**
+     * Método responsável por verificar se o request possui filtros válidos
+     * e montar os filtros
+     */
+    public function montaFiltros($request)
+    {
+        $filtros = [];
+        for ($i = 0; $i < count($request); $i++) { 
+            if (
+                isset($request[$i]) &&
+                isset($request[$i+1]) &&
+                isset($request[$i+2]) &&
+                isset($request[$i][0]) &&
+                isset($request[$i+1][0]) &&
+                isset($request[$i+2][0]) &&
+                isset($request[$i][1]) &&
+                isset($request[$i+1][1]) &&
+                isset($request[$i+2][1]) &&
+                $request[$i][0]   === "filtro_campo" &&
+                $request[$i+1][0] === "filtro_comparacao" &&
+                $request[$i+2][0] === "filtro_texto"
+            ) {
+                $filtros[] = [
+                    'campo'      => $request[$i][1],
+                    'comparacao' => $request[$i+1][1],
+                    'texto'      => $request[$i+2][1]
+                ];
+            } else {
+                return false;
+            }
+            $i = $i + 2;
+        }
+        return $filtros;
+    }
+
+    /**
+     * Método responsável por validar se cada filtro foi preenchido corretamente
+     */
+    public function validaFiltros($filtros)
+    {
+        $tiposFiltrosCampo = ["titulo", "autor", "assunto", "data_publicacao"];
+        $tiposFiltrosComparacao = ["contem", "igual", "nao_contem"];
+
+        for ($i = 0; $i < count($filtros); $i++) {
+            $element = $filtros[$i];
+
+            if (
+                !in_array($element["campo"], $tiposFiltrosCampo) ||
+                !in_array($element["comparacao"], $tiposFiltrosComparacao)
+            ) {
+                return false;
+            }
+            // Validação especial para o tipo data
+            if ($element["campo"] === $tiposFiltrosCampo[3]) {
+                // Permitido apenas: XXXX (Ex: 2020) ou XXXX-XXXX (2017-2020)
+                $pattern = "/^[0-9]{4}(\-[0-9]{4}|)$/";
+                $resultado = preg_match($pattern, $element["texto"], $matches);
+                if (!$resultado) {
+                    return false;
+                }
+            }
+        };
+        return true;
     }
 }
